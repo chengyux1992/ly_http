6b1ee0a599c5cc372380e1474a8a1a8aaf61fa24
chengyux1992 <103913134+chengyux1992@users.noreply.github.com>
2025-02-24 23:51:55 +0800

    Update site.ts

--- next-web-nav-main/config/site.ts
+++ next-web-nav-main/config/site.ts
@@ -95,7 +95,7 @@ export const NavData: NavData[] = [
 
  
   {
-    title: "字体图标",
+    title: "图片素材",
     items: [
       {
         title: "iconify",
@@ -103,61 +103,7 @@ export const NavData: NavData[] = [
         icon: "https://icon-sets.iconify.design/favicon.ico",
         link: "https://icon-sets.iconify.design/"
       },
-      {
-        title: "icones",
-        desc: "Icon Explorer with Instant searching, powered by Iconify",
-        icon: "https://icones.js.org/favicon.svg",
-        link: "https://icones.js.org/"
-      },
-      {
-        title: "iconfont",
-        desc: "iconfont-国内功能很强大且图标内容很丰富的矢量图标库，提供矢量图标下载、在线存储、格式转换等功能",
-        icon: "https://img.alicdn.com/imgextra/i4/O1CN01EYTRnJ297D6vehehJ_!!6000000008020-55-tps-64-64.svg",
-        link: "https://www.iconfont.cn/"
-      },
-      {
-        title: "feathericons",
-        desc: "简单美丽的开源图标",
-        icon: "https://feathericons.com/favicon.ico",
-        link: "https://feathericons.com/"
-      },
-      {
-        title: "undraw",
-        desc: "一个不断更新的设计项目与美丽的SVG图像，使用完全免费",
-        icon: "https://undraw.co/apple-touch-icon.png",
-        link: "https://undraw.co/"
-      },
-      {
-        title: "igoutu",
-        desc: "图标、插图、照片、音乐和设计工具",
-        icon: "/icons/igoutu.png",
-        link: "https://igoutu.cn/"
-      },
-      {
-        title: "Emojiall",
-        desc: "Emoji表情大全",
-        icon: "https://www.emojiall.com/apple-touch-icon.png",
-        link: "https://www.emojiall.com/zh-hans"
-      },
-      {
-        title: "渐变色网站",
-        desc: "数百万个自动生成的渐变的网站",
-        icon: "https://gradihunt.com/favicon.ico",
-        link: "https://gradihunt.com/"
-      },
-      {
-        title: "谷歌字体",
-        desc: "一个生成渐变色背景的网站",
-        icon: "/icons/google_fonts.ico",
-        link: "https://googlefonts.cn/"
-      },
-      {
-        title: "Typing SVG",
-        desc: "一个动态生成的可自定义 SVG 打字效果",
-        icon: "/icons/typing-svg.png",
-        link: "https://readme-typing-svg.herokuapp.com/demo/"
-      }
-    ]
+     ]
   },
   {
     title: "跨平台",
@@ -194,57 +140,6 @@ export const NavData: NavData[] = [
 
     ]
   },
-  {
-    title: "网站托管",
-    items: [
-      {
-        title: "Vercel",
-        desc: "Vercel将最好的开发人员体验与对最终用户性能的执着关注相结合",
-        icon: "https://assets.vercel.com/image/upload/q_auto/front/favicon/vercel/57x57.png",
-        link: "https://vercel.com"
-      },
 
-    ]
-  },
-  {
-    title: "社区",
-    items: [
-      {
-        icon: "https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon.png?v=c78bd457575a",
-        title: "Stack Overflow",
-        desc: "全球最大的技术问答网站",
-        link: "https://stackoverflow.com"
-      },
-      {
-        title: "稀土掘金",
-        icon: "https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web//static/favicons/apple-touch-icon.png",
-        desc: "面向全球中文开发者的技术内容分享与交流平台",
-        link: "https://juejin.cn"
-      },
-      {
-        title: "V2EX",
-        icon: "https://www.v2ex.com/static/icon-192.png",
-        desc: "一个关于分享和探索的地方",
-        link: "https://www.v2ex.com"
-      },
-      {
-        title: "SegmentFault 思否",
-        icon: "https://static.segmentfault.com/main_site_next/0dc4bace/touch-icon.png",
-        desc: "技术问答开发者社区",
-        link: "https://segmentfault.com"
-      },
-      {
-        title: "博客园",
-        icon: "/icons/cnblogs.svg",
-        desc: "博客园是一个面向开发者的知识分享社区",
-        link: "https://www.cnblogs.com"
-      },
-      {
-        title: "知乎",
-        icon: "https://static.zhihu.com/heifetz/assets/apple-touch-icon-60.362a8eac.png",
-        desc: "中文互联网高质量的问答社区和创作者聚集的原创内容平台",
-        link: "https://zhihu.com"
-      }
-    ]
-  }
+
 ]
